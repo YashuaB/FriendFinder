0f0e8ed1d6dad1268695e87e15e76da9a4c3e88a
yashua <yashuab.abraham@gmail.com>
2019-04-20 22:41:06 -0400

    fixed broken post in survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,31 @@
 
 
 var friends = require("../data/friends")
-
+// var friendPlus = require("../public/survey.html")
 
 module.exports = function(app){
-  
+
   app.get("/api/friends", function(req, res) {
    res.json(friends)
   });
   
   
-  app.post("/api/friends", function(req, res) {
-   
+  app.post("/api/friends", function(req,res){
+
+    friends.push(req.body)
+    console.log(req.body)
+    console.log(friends)
+    res.json(true)
+
     
-  });
+    
+  })
+
+
+  // .then(function(data){
+  //   console.log("survey.html", data)
+  //   console.log("Adding friends...")
+  // })
   
   
 }
