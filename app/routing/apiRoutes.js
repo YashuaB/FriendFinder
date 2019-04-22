223c068d1d47a4e62820b62ff3f75669d799f694
yashua <yashuab.abraham@gmail.com>
2019-04-22 13:45:30 -0400

    add basic text to home page and linked button to survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,20 +6,35 @@ var friends = require("../data/friends")
 module.exports = function(app){
 
   app.get("/api/friends", function(req, res) {
+
    res.json(friends)
+   console.log(req.body)
   });
   
   
   app.post("/api/friends", function(req,res){
 
-    friends.push(req.body)
+    var newFriend = req.body
+    
+    for( var i = 0 ; i < friends.length;i++){
+
+      if(newFriend.score[i] !== friends[i].score[i])
+      newFriend.score[i] - friends[i].score[i]
+    }
+
+
+
+    friends.push(newFriend)
     console.log(req.body)
-    console.log(friends)
+   
+    res.json(newFriend)
     res.json(true)
 
     
     
   })
+
+// if value[i] of arr1 is not equal to value[i] of arr2 then add those values and push new value to a new array
 
 
   // .then(function(data){
